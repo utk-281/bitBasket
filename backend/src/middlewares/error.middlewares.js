c69e0f3dcbc7a5735857c01d2b257306a87608e1
utkarsh <utkarshgupta.281@gmail.com>
2025-07-03 14:00:27 +0530

    first commit

---
+++ backend/src/middlewares/error.middlewares.js
@@ -0,0 +1,12 @@
+const error = (err, req, res, next) => {
+  err.message = err.message || "Internal Server Error";
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+    errObject: err,
+  });
+};
+
+module.exports = error;
